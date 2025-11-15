dc2afc40f5328a793912c766919f1d723cba7257
irham <i.maulana.azn@gmail.com>
2025-11-15 19:49:05 +0700

    minor change - ubah judul form pembelian

--- app/Views/pages/owner/pembelian/index.php
+++ app/Views/pages/owner/pembelian/index.php
@@ -5,7 +5,7 @@
     <div class="w-full max-w-full px-3 mx-auto mt-0 md:flex-0 shrink-0 md:w-7/12 lg:w-5/12 xl:w-4/12">
         <div class="relative z-0 flex flex-col min-w-0 break-words bg-white border-0 shadow-soft-xl rounded-2xl bg-clip-border">
             <div class="p-6 pb-0 mb-0 text-center bg-white border-b-0 rounded-t-2xl">
-                <h5 class="">Tambah Pengeluaran</h5>
+                <h5 class="">Catat Pengeluaran</h5>
             </div>
 
             <div class="flex-auto p-6">
