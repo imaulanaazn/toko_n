9414779a3010b925f83ea465b6d9e77f3776225c
irham <i.maulana.azn@gmail.com>
2025-11-19 10:17:21 +0700

    change base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     // public string $baseURL = 'http://localhost:8080/';
-    public string $baseURL = 'https://warung_n.zeabur.app/';
+    public string $baseURL = 'https://warung-n.zeabur.app/';
 
 
     /**
